a4863dbfc9be9893476d624fea9448bfebaf597b
AliMuhammadAslam <35271095+AliMuhammadAslam@users.noreply.github.com>
2023-06-01 23:42:35 +0500

    Delete front-end-ar-marketing-hub directory

--- front-end-ar-marketing-hub/src/Pages/ErrorPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function ErrorPage() {
-    return (
-    <div style={{fontFamily:'Futura-bold', fontSize:'60px'}}>ERROR! PAGE NOT FOUND</div>
-    )
-}
-
-export default ErrorPage;
