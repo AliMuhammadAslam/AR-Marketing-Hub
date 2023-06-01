6aa32c3b6f4f57c95ce19348a74eb77504ba1e71
AliMuhammadAslam <35271095+AliMuhammadAslam@users.noreply.github.com>
2023-06-01 23:42:48 +0500

    Delete project-apis-ar-marketing-hub directory

--- project-apis-ar-marketing-hub/Models/home.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-//passportLocalMongoose = require("passport-local-mongoose");
-
-const homeSchema = new mongoose.Schema({
-    Image:  String,
-    text: String,
-})
-
-module.exports =  mongoose.model("Home", homeSchema);
--- project-apis-ar-marketing-hub/README.md
+++
@@ -1,8 +0,0 @@
-# project-apis-ar-marketing-hub
-project-apis-ar-marketing-hub created by GitHub Classroom
-
-APIs for Web Dev Project "AR Marketing Hub"
-
-Developed by:<br />
-Abdur Rafai Tariq - 18583<br />
-Ali Muhammad      - 18585
